5fc88b4cf816db79a578f6af072fa15c0eaf0fc5
Leo <fleonardolewis@gmail.com>
2020-08-29 16:26:26 -0500

    inital commit

---
+++ projects/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+
+from django.contrib import admin
+from projects.models import Project
+
+class ProjectAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Project, ProjectAdmin)
+
+
